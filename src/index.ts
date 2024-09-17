f1e39bc700ed2a505661b19b03147c47411b778a
Nallib <nallib@termatrac.com>
2024-09-17 13:08:34 +1000

    testing

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// src/index.ts
+
+// Re-export everything from your subdirectories
+//@ts-ignore
+// export * from './services';
+// //@ts-ignore
+// export * from './jira';
+// //@ts-ignore
+// export * from './components';
+//@ts-ignore
+export * from './lib/*';
+//@ts-ignore
+export * from './types/*';
